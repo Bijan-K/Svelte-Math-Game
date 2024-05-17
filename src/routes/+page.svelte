32e6ab757820c2d1b962625561420529a11b6874
Bijan k <80640045+Bijan-K@users.noreply.github.com>
2024-05-17 10:28:07 +0330

    Set basic routes, Add new icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-primary ">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <title>Math Game | Home</title>
+    <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<h1 class="text-primary">Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
